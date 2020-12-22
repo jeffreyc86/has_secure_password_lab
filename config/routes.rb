6e81604abc566b96fd6de66dde1113d2c52391df
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-12-22 18:42:08 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get 'sessions/new', to: 'sessions#new', as: 'new_session'
+  post '/sessions', to: 'sessions#create'
+
+  get 'welcome/home', to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
